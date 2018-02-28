187ac896089f74ee05c06555c0e2b9ccf775385c
MagicLegend <jort@elska.nl>
2018-02-28 22:11:18 +0100

    Added cliche lib and sample code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import asg.cliche.Command;
+import asg.cliche.ShellFactory;
+import asg.cliche.example.HelloWorld;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    @Command // One,
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @Command // two,
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Command
+    public String temp(int a) {
+        return "Woo " + a;
+    }
+
+    /**
+     * For lib usage: https://code.google.com/archive/p/cliche/wikis/Manual.wiki
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        ShellFactory.createConsoleShell("hello", "", new Main())
+                .commandLoop(); // and three.
     }
 }
