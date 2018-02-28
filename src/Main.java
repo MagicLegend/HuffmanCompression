4af4e25c440e1734a5d3d4cbe8378c1d04b2c5c4
MagicLegend <jort@elska.nl>
2018-02-28 22:27:46 +0100

    Removed comments, and moved the temp function around

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,6 @@ import java.io.IOException;
 
 public class Main {
 
-    @Command // One,
-    public String hello() {
-        return "Hello, World!";
-    }
-
-    @Command // two,
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    @Command
-    public String temp(int a) {
-        return "Woo " + a;
-    }
 
     /**
      * For lib usage: https://code.google.com/archive/p/cliche/wikis/Manual.wiki
@@ -29,6 +15,12 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         ShellFactory.createConsoleShell("hello", "", new Main())
-                .commandLoop(); // and three.
+                .commandLoop();
     }
+
+    @Command
+    public String temp(int a) {
+        return "Woo " + a;
+    }
+
 }
